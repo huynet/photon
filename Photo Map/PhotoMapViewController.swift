f09d38e7076996b3e92dfc0824f6950668acb723
Huy Pham <cocwedc@gmail.com>
2018-10-12 11:31:08 -0500

    beta

--- Photo Map/PhotoMapViewController.swift
+++ Photo Map/PhotoMapViewController.swift
@@ -7,13 +7,59 @@
 //
 
 import UIKit
+import MapKit
 
-class PhotoMapViewController: UIViewController {
-
+class PhotoMapViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var picture: UIImage!
+    
+    @IBAction func onChoose(_ sender: UIButton) {
+        let vc = UIImagePickerController()
+        vc.delegate = self
+        vc.allowsEditing = true
+        
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            print("Camera is available")
+            vc.sourceType = .camera
+        } else {
+            print("Camera is not available so we are using photo library instead")
+            vc.sourceType = .photoLibrary
+        }
+        present(vc, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        picture = info[UIImagePickerControllerOriginalImage] as! UIImage
+        
+        dismiss(animated: true, completion: {self.performSegue(withIdentifier: "tagSegue", sender: nil)})
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//        let address = "Mountain View, California"
+//        getCoordinate(addressString: address) { success in
+//            let addr = success.0
+//            self.mapView.region = MKCoordinateRegionMakeWithDistance(addr, 2000, 2000)
+//        }
+        let sfRegion = MKCoordinateRegionMake(CLLocationCoordinate2DMake(37.7833, -122.4167), MKCoordinateSpanMake(0.1, 0.1))
+        mapView.setRegion(sfRegion, animated: false)
+    }
+    
+    func getCoordinate(addressString: String, completionHandler: @escaping(CLLocationCoordinate2D, NSError?) -> Void) {
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(addressString) { (placemarks, error) in
+            if error == nil {
+                if let placemark = placemarks?[0] {
+                    let location = placemark.location!
+                    
+                    completionHandler(location.coordinate, nil)
+                    return
+                }
+            }
+            completionHandler(kCLLocationCoordinate2DInvalid, error as NSError?)
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,7 +75,9 @@ class PhotoMapViewController: UIViewController {
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        if segue.identifier == "tagSegue" {
+            let locationsViewController = segue.destination as! LocationsViewController
+            locationsViewController.delegate =  self
+        }
     }
-    
-
 }
